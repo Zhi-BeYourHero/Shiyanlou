a7b9e08c32aea95fd299c5c60f451d8b011d5855
Zhi-BeYourHero <763378665@qq.com>
2017-12-18 23:53:24 +0800

    Challenge three

--- calculator.py
+++ calculator.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
 import sys
 try:
+    filename='/home/shiyanlou/test.csv'
+    with open(filename) as file:
+        d={}
+        for i in file.readlines():
+            e=i.strip().split('=')
+            key,value=e[0],e[1]
+            d[key]=value
+        print(d)
+
+
     for arg in sys.argv[1:]:
         b=arg.split(':')
         Salary=int(b[1])
-        Fiof=Salary*0.165
+    if Salary<JiShuH:
+        Fiof=Salary*(YangLao+YiLiao+ShiYe+GongShang+ShengYu+GongJiJin)
         Rest=(Salary-3500-Fiof)
+        if Salary<2193:
+            
         if Rest<0:
             Tax=0
         if 0<Rest<1500:
@@ -22,7 +35,7 @@ try:
             Tax=Rest*0.35-5505
         if 80000<=Rest:
             Tax=Rest*0.45-13505
-        Income=format((Salary-Fiof-Tax),".2f")
+        Income=format((Salary-Fiof-Tax-SocIns),".2f")
         print(b[0]+':'+Income)
 
     
