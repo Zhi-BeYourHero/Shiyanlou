7dd2db0ae0c6dd6a426a8af676efed2dd570f165
Zhi-BeYourHero <763378665@qq.com>
2017-12-21 15:55:43 +0800

    challenge2

--- calculator.py
+++ calculator.py
@@ -1,43 +1,79 @@
 #!/usr/bin/env python3
-import sys
-try:
-    filename='/home/shiyanlou/test.csv'
-    with open(filename) as file:
-        d={}
-        for i in file.readlines():
-            e=i.strip().split('=')
-            key,value=e[0],e[1]
-            d[key]=value
-        print(d)
+from collections import namedtuple
+IncomeTaxQuickLookupItem=namedtuple(
+    'IncomeTaxQuickLookupItem',
+    ['start_point','tax_rate','quick_subtractor']
+)
+
+INCOME_TAX_START_POINT=3500
+
+INCOME_TAX_QUICK_LOOKUP_TABLE=[
+    'IncomeTaxQuickLookupItem'=(80000,0.45,13505),
+    'IncomeTaxQuickLookupItem'=(55000,0.35,5505),
+    'IncomeTaxQuickLookupItem'=(35000,0.30,2755),
+    'IncomeTaxQuickLookupItem'=(9000,0.25,1005),
+    'IncomeTaxQuickLookupItem'=(4500,0.20,555),
+    'IncomeTaxQuickLookupItem'=(1500,0.10,105),
+    'IncomeTaxQuickLookupItem'=(0,0.03,0)
+]
+
+SOCIAL_INSURANCE_MONEY_RATE={
+    'endowment_insurance':0.08,
+    'medical_insurance':0.02,
+    'unemployment_insurance':0.005,
+    'employment_injury_insurance':0,
+    'maternity_insurance':0,
+    'public_accumulation_funds':0.06
+}
+
+def calc_income_tax_and_remain(income):
+    social_insurance_money=income*sum(SOCIAL_INSURANCE_MONEY_RATE.values())
+    real_income=income-social_insurance_money
+    taxable_part=real_income-INCOME_TAX_START_POINT
+    if taxable_part<=0:
+        return '0.00' '{:.2f}'.format(real_income)
+    for item in INCOME_TAX_QUICK_LOOKUP_TABLE:
+        if taxable_part>item.start.point:
+            tax=taxable_part*item.tax_rate-item.quick_subtractor
+            return '{:.2f}'.format(tax),'{:2f}'.format(real_income-tax)
 
 
-    for arg in sys.argv[1:]:
-        b=arg.split(':')
-        Salary=int(b[1])
-    if Salary<JiShuH:
-        Fiof=Salary*(YangLao+YiLiao+ShiYe+GongShang+ShengYu+GongJiJin)
-        Rest=(Salary-3500-Fiof)
-        if Salary<2193:
-            
-        if Rest<0:
-            Tax=0
-        if 0<Rest<1500:
-            Tax=Rest*0.03
-        if 1500<=Rest<4500:
-            Tax=Rest*0.1-105
-        if 4500<=Rest<9000:
-            Tax=Rest*0.2-555
-        if 9000<=Rest<35000:
-            Tax=Rest*0.25-1005
-        if 35000<=Rest<55000:
-            Tax=Rest*0.3-2755
-        if 55000<=Rest<80000:
-            Tax=Rest*0.35-5505
-        if 80000<=Rest:
-            Tax=Rest*0.45-13505
-        Income=format((Salary-Fiof-Tax-SocIns),".2f")
-        print(b[0]+':'+Income)
+def main():
+    for item in sys.argv[1:]:
+        employee_id,income_string=item.split(':')
+        try:
+            income=int(income_string)
+        except ValueError:
+            print("Parameter Error")
+        _,remain=calc_income_tax_and_remain(income)
+        print('{}:{}'.format(employee_id,remain))
 
-    
-except:
-    print("Parameter Error")
+if __name__='__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
